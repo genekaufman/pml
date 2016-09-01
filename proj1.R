d4abfb10e3cca2c35e4078dc798a5958e070c7d3
Gene Kaufman <genekaufman@gmail.com>
2016-09-01 13:08:52 -0400

    csv read

--- proj1.R
+++ proj1.R
@@ -1,17 +1,20 @@
+#https://rstudio-pubs-static.s3.amazonaws.com/29426_041c5ccb9a6a4bedb204e33144bb0ad4.html
+
 library(caret)
 library(AppliedPredictiveModeling)
 
 rm(list = ls())
 
 training_csv <- "pml-training.csv";
-RawData <- read.csv(training_csv, colClasses = "character");
+#RawData <- read.csv(training_csv, colClasses = "character");
+RawData <- read.csv(training_csv, na.strings=c("NA","#DIV/0!",""));
 RawData$classe <- as.factor(RawData$classe);
+RawData$user_name <- as.factor(RawData$user_name);
+RawData$new_window <- as.factor(RawData$new_window);
 
-data(AlzheimerDisease)
 
-adData = data.frame(diagnosis,predictors)
-trainIndex1 = createDataPartition(adData,p=0.75,list=FALSE);
-#RawDataDF = data.frame(RawData);
+set.seed(42);
+
 trainIndex = createDataPartition(y=RawData$classe,p=0.75,list=FALSE);
-#training = RawData[trainIndex,];
-#validation = RawData[-trainIndex,];
+training = RawData[trainIndex,];
+validation = RawData[-trainIndex,];
